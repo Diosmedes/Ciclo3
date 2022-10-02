8cb6d7393000b2004370b0697ccbbcfa493dc01e
Diosmedes <111157281+Diosmedes@users.noreply.github.com>
2022-10-02 13:12:38 -0500

    Funciones Crud de la tabla clientes
    Cambios en el controladorinicio, paginas  de html agregadas

--- src/main/java/com/co/ControladorInicioTuristic.java
+++ src/main/java/com/co/ControladorInicioTuristic.java
@@ -2,10 +2,14 @@
 package com.co;
 
 import com.co.DAO.ClienteDAO;
+import com.co.POJO.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -27,5 +31,27 @@ public class ControladorInicioTuristic {
         var clientes= clientesDao.findAll();
         model.addAttribute("clientes", clientes);
         return "loginadmin";
+    }
+     @GetMapping("/crear")
+    public String crear()
+    {
+        return "crear";
+    }
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute  Cliente clientes)
+    {
+        clientesDao.save(clientes);
+        return"redirect:/loginadmin";
+    }
+     @GetMapping("/editar/{idcliente}")
+    public String editar(@PathVariable("idcliente") Integer idcliente, Model model){
+        var clientes= clientesDao.findById(idcliente);
+        model.addAttribute("clientes",clientes);
+        return "modificar";
+    }
+    @GetMapping("/eliminar/{idcliente}")
+    public String eliminar(@PathVariable("idcliente") Integer idcliente){
+        clientesDao.deleteById(idcliente);
+        return "redirect:/loginadmin";
     }
 }
